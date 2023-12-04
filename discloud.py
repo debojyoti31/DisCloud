6c49262c808cde222f3f6fe61bd40b799f043742
Debojyoti Mondal <97456082+debojyoti31@users.noreply.github.com>
2023-12-04 16:09:20 +0530

    v2.1
    v2.1

--- discloud.py
+++ discloud.py
@@ -4,9 +4,18 @@ import discord
 import random
 from discord.ext import commands
 from cryptography.fernet import Fernet
+import math
+import io
+import pytz
 
 # Set 'BOT_TOKEN' with your actual bot token
-BOT_TOKEN = 'MTE3ODM2kjhgfewrtyrutyilu;oilkjh0oEchtyjklj'
+BOT_TOKEN = 'your_bot_token'
+
+# Set the Chunk Size
+chunk_size = 15 * 1024 * 1024
+
+# Set your Time Zone 
+your_timezone = 'Asia/Kolkata'
 
 path_separator = os.path.sep
 
@@ -40,9 +49,6 @@ async def split(ctx, *, file_path: str):
     Split a file into parts and upload them to the Discord server.
     Usage: !split <file_path>
     """
-    # Set the Chunk Size
-    chunk_size = 15 * 1024 * 1024
-
     file_name = file_path.split(path_separator)[-1]
 
     # Check if the file has already been uploaded to Discord
@@ -50,15 +56,9 @@ async def split(ctx, *, file_path: str):
         await ctx.send(f"File '{file_name}' has already been uploaded.")
         return
 
-    
     if not os.path.exists(file_path):
         await ctx.send(f"File '{file_name}' not found.")
         return
-    
-    # file_name, file_extension = os.path.splitext(file_name)
-    output_folder = os.path.join("file_parts", f"{file_name}_parts")
-    os.makedirs(output_folder, exist_ok=True)
-
 
     part_num = 1
 
@@ -71,16 +71,9 @@ async def split(ctx, *, file_path: str):
                 if not chunk:
                     break
 
-                part_file = os.path.join(output_folder, f"{file_name}_part{part_num}.txt")
-                with open(part_file, 'wb') as part:
-                    part.write(encrypted_chunk)
-
                 # Upload the part to Discord
-                with open(part_file, 'rb') as part_to_upload:
-                    await ctx.send(f"Uploading part {part_num}/{total_parts} of: {file_name}")
-                    await ctx.send(file=discord.File(part_to_upload, filename=f"{file_name}_part{part_num}.txt"))
-
-                os.remove(part_file)
+                await ctx.send(f"Uploading part {part_num}/{total_parts} of: {file_name}")
+                await ctx.send(file=discord.File(io.BytesIO(encrypted_chunk), filename=f"{file_name}_part{part_num}.txt"))
                 part_num += 1
 
         await ctx.send(f"{file_name} has been split into parts and uploaded.")
@@ -88,9 +81,6 @@ async def split(ctx, *, file_path: str):
         await ctx.send(f"'{file_path}' is a directory, NOT a File! ")
     except Exception as e:
         await ctx.send(f"An error occurred: {e}")
-    finally:
-        # Clean up: Delete the 'file_parts' directory
-        shutil.rmtree(output_folder, ignore_errors=True)
 
 
 @bot.command(name='combine', help='Download the files from Discord and Combine Them.')
@@ -101,9 +91,15 @@ async def combine(ctx, *, combined_file_path: str):
     """
 
     file_name = combined_file_path.split(path_separator)[-1]
+
     # Check if the file has already been uploaded to Discord
     if not await is_file_uploaded(ctx, file_name):
         await ctx.send(f"File '{file_name}' not found.")
+        return
+    
+    # Check if the combined file already exists
+    if os.path.exists(combined_file_path):
+        await ctx.send(f"File **{file_name}** already exists at **{combined_file_path.rsplit(path_separator, 1)[0]}**.\nPlease choose a different output path.")
         return
 
     # Download file parts from Discord
@@ -124,6 +120,7 @@ async def combine(ctx, *, combined_file_path: str):
         # Clean up: Delete the temporary folder
         shutil.rmtree(parts_folder, ignore_errors=True)
 
+
 @bot.command(name='delete', help='Delete parts of uploaded file.')
 async def delete(ctx, *, file_name: str):
     """
@@ -139,7 +136,37 @@ async def delete(ctx, *, file_name: str):
     await delete_file_parts(ctx, file_name)
 
     await ctx.send(f"All file parts for '{file_name}' have been deleted.")
-    
+
+
+@bot.command(name='listfiles', help='Get all the uploaded files.')
+async def listfiles(ctx):
+    """
+    Display a list of all uploaded files in the Discord channel.
+    Usage: !listfiles
+    """
+    uploaded_files = await get_uploaded_files(ctx)
+
+    if not uploaded_files:
+        await ctx.send("No files have been uploaded.")
+    else:
+        file_list = "\n".join(f"**{index + 1}.** {file}" for index, file in enumerate(uploaded_files))
+        await ctx.send(f"**Uploaded Files:**\n{file_list}")
+
+
+@bot.command(name='filedetails', help='Get details of an uploaded file.')
+async def filedetails(ctx, *, file_name: str):
+    """
+    Display details of a specific uploaded file in the Discord channel.
+    Usage: !details <file_name>
+    """
+    file_details = await get_file_details(ctx, file_name)
+
+    if not file_details:
+        await ctx.send(f"File '{file_name}' not found.")
+    else:
+        await ctx.send(f"**Details for:** '{file_name}'\n{file_details}")
+
+
 def combine_parts(combined_file_path, parts_folder):
     file_name = combined_file_path.split(path_separator)[-1]
 
@@ -160,6 +187,7 @@ def combine_parts(combined_file_path, parts_folder):
 
             part_num += 1
 
+
 async def download_file_parts(ctx, combined_file_path):
     file_name = combined_file_path.rsplit(path_separator,1)[1]
     # Create a temporary folder to store downloaded file parts
@@ -176,6 +204,7 @@ async def download_file_parts(ctx, combined_file_path):
 
     return parts_folder
 
+
 async def is_file_uploaded(ctx, file_name):
     # Search for messages in the channel with attachments matching the file name
     async for message in ctx.channel.history(limit=None):
@@ -184,6 +213,7 @@ async def is_file_uploaded(ctx, file_name):
                 return True
     return False
 
+
 async def delete_file_parts(ctx, file_name):
     # Search for messages in the channel with file parts matching the file name
     async for message in ctx.channel.history(limit=None):
@@ -191,5 +221,59 @@ async def delete_file_parts(ctx, file_name):
             if attachment.filename.rsplit('_', 1)[0] == file_name:
                 await ctx.send(f"Deleting part {attachment.filename}")
                 await message.delete()
+
+
+async def get_uploaded_files(ctx):
+    """
+    Retrieve a list of uploaded original files in the Discord channel.
+    """
+    uploaded_files = set()
+
+    async for message in ctx.channel.history(limit=None):
+        for attachment in message.attachments:
+            file_name = attachment.filename
+            # Extract the original file name
+            original_file_name = file_name.rsplit('_', 1)[0]
+            uploaded_files.add(original_file_name)
+
+    return uploaded_files
+
+
+def format_size(size_in_bytes):
+    """
+    Format the size in bytes to a human-readable format.
+    """
+    if size_in_bytes == 0:
+        return "0 B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_in_bytes, 1024)))
+    p = math.pow(1024, i)
+    size = round(size_in_bytes / p, 2)
+    return f"{size} {size_name[i]}"
+
+
+async def get_file_details(ctx, file_name):
+    """
+    Retrieve details of a specific uploaded file in the Discord channel.
+    """
+    total_parts, total_size, last_uploaded_timestamp = 0, 0, None
+
+    async for message in ctx.channel.history(limit=None):
+        for attachment in message.attachments:
+            if attachment.filename.rsplit('_', 1)[0] == file_name:
+                total_parts += 1
+                total_size += attachment.size
+                current_timestamp = message.created_at.replace(tzinfo=pytz.UTC)
+                if last_uploaded_timestamp is None or current_timestamp > last_uploaded_timestamp:
+                    last_uploaded_timestamp = current_timestamp
+
+    if total_parts == 0:
+        return None
+
+    formatted_size = format_size(total_size)
+    last_uploaded_timestamp_local = last_uploaded_timestamp.astimezone(pytz.timezone(your_timezone))  # Replace 'Your/Timezone' with your actual time zone
+
+    return f"**Total Parts:** {total_parts}\n**Total Size:** {formatted_size}\n**Last Uploaded at:** {last_uploaded_timestamp_local.strftime('%Y-%m-%d %H:%M:%S %Z')}"
+
 
 bot.run(BOT_TOKEN)
