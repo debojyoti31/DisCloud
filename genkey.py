7ce48be7b10b8b413578dff66c40fb3afc2ee95a
Debojyoti Mondal <97456082+debojyoti31@users.noreply.github.com>
2023-12-04 16:11:28 +0530

    Update genkey.py

--- genkey.py
+++ genkey.py
@@ -1,16 +1,16 @@
 from cryptography.fernet import Fernet
 
-def generate_fernet_key():
+def generate_fernet_key(key_name):
     """
     Generate a Fernet key and save it to a file.
     """
     key = Fernet.generate_key()
 
-    with open("enckey.key", 'wb') as key_file:
+    with open(key_name, 'wb') as key_file:
         key_file.write(key)
 
     print("Fernet key generated and saved to 'enckey.key'.")
 
 if __name__ == "__main__":
-    generate_fernet_key()
+    generate_fernet_key('enckey.key')
 
