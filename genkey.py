47edcc6ad75fce09056b6611490a14973a89d052
Debojyoti Mondal <97456082+debojyoti31@users.noreply.github.com>
2023-12-05 12:20:24 +0530

    Update genkey.py

--- genkey.py
+++ genkey.py
@@ -1,9 +1,22 @@
 from cryptography.fernet import Fernet
+import os
 
 def generate_fernet_key(key_name):
     """
-    Generate a Fernet key and save it to a file.
+    Generate a Fernet key and save it to a file, asking the user twice if they want to override an existing file.
     """
+    if os.path.exists(key_name):
+        user_response = input(f"{key_name} already exists. Do you want to override it? (yes/no): ").lower()
+        if user_response != 'yes':
+            print("Key generation aborted.")
+            return
+        if user_response == 'yes':
+            # Double-check with the user
+            double_check = input("\nAre you really sure you want to generate a new key and override the existing file? \n If YES, write 'I WANT TO OVERRIDE THE KEY' and press Enter \n : ").lower()
+            if double_check.lower() != 'i want to override the key':
+                print("Key generation aborted.")
+                return
+
     key = Fernet.generate_key()
 
     with open(key_name, 'wb') as key_file:
@@ -13,5 +26,3 @@ def generate_fernet_key(key_name):
 
 if __name__ == "__main__":
     generate_fernet_key('enckey.key')
-    
-
